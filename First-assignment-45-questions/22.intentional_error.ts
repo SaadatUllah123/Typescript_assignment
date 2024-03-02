8e1b349e20b5c74b1e54e8e6e54ffbc2a59351f0
SaadatUllah123 <159489971+SaadatUllah123@users.noreply.github.com>
2024-03-02 14:42:45 -0800

    Add files via upload

---
+++ First-assignment-45-questions/22.intentional_error.ts
@@ -0,0 +1,8 @@
+let names : string[] = ["nematullah","saadatullah","m.sajjad"];
+
+// incorrect the accessing in the array by index
+console.log(names[3]);
+
+// correct the accessing in the array by index
+console.log(names[1]);
+
---
+++ First-assignment-45-questions/23.conditional_tests.ts
@@ -0,0 +1,42 @@
+let car = 'subaru';
+
+// 1 tests evaluate to True
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// 2 tests evaluate to True
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+
+// 3 tests evaluate to True
+console.log("Is car !== 'subaru'? I predict True.");
+console.log(car !== 'bike');
+
+// 4 tests evaluate to True
+console.log("Is car.upppercase() == 'subaru'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// 5 tests evaluate to True
+
+console.log("Is car != 'subaru'? I predict True.");
+console.log(car != 'bus');
+
+// 6 tests evaluate to false
+console.log("Is car != 'subaru'? I predict false.");
+console.log(car != 'subaru');
+
+// 7 tests evaluate to false
+console.log("Is car == 'bus'? I predict false.");
+console.log(car == 'bus');
+
+// 8 tests evaluate to false
+console.log("Is car !=='subaru'? I predict false.");
+console.log(car !=='subaru');
+
+// 9 tests evaluate to false
+console.log("Is car.touppercase() === 'subaru'? I predict false.");
+console.log(car.toUpperCase() === 'subaru');
+
+// 10 tests evaluate to false
+console.log("Is car === 'subaru'? I predict false.");
+console.log(car === 'bus');
