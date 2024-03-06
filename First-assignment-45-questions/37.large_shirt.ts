d89d56104b68c3b8b3ea9567ac2345f610c889cc
SaadatUllah123 <159489971+SaadatUllah123@users.noreply.github.com>
2024-03-06 13:30:28 -0800

    Add files via upload

---
+++ First-assignment-45-questions/37.large_shirt.ts
@@ -0,0 +1,9 @@
+function  make_shirt(size:string = "large", messsage:string = "I love typescript"):void{
+    console.log(`size: ${size} and message: ${messsage} `);
+};
+
+make_shirt();
+make_shirt("medium");
+make_shirt("small")
+
+
