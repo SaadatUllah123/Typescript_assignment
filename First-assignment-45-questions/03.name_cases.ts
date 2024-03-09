57ad881510b6e1e033a39acd1bfcbfa548b3f3a1
SaadatUllah123 <159489971+SaadatUllah123@users.noreply.github.com>
2024-03-09 12:50:04 -0800

    Delete First-assignment-45-questions/03.name_cases.ts

--- First-assignment-45-questions/03.name_cases.ts
+++
@@ -1,5 +0,0 @@
-let person_name : string = "Saadatullah";
-
-console.log(`lowercase: ${person_name.toLowerCase()}`);
-console.log(`uppercase: ${person_name.toUpperCase()}`);
-console.log(`titlecase: ${person_name}`)
