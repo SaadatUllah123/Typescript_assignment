e6492b708b014fbf867f22df52ccea99714390e9
SaadatUllah123 <159489971+SaadatUllah123@users.noreply.github.com>
2024-03-09 12:51:44 -0800

    Add files via upload

---
+++ First-assignment-45-questions/03.name_cases.ts
@@ -0,0 +1,5 @@
+let person_name : string = "saadatullah";
+
+console.log("lowercase:",person_name.toLowerCase());
+console.log("uppercase:",person_name.toUpperCase());
+console.log("titlecase:",person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
